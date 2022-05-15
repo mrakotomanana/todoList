a7cec63d66be8c598b0bc54c200aecc682c7bbae
mrakotomanana <rakotomanan@gmail.com>
2022-05-15 15:40:10 +0200

    fichier ignore

---
+++ demo.js
@@ -0,0 +1,9 @@
+// var ajax = function(url){
+//     var xhr = XMLHttpRequest()
+//     xhr.open('GET', url, true)
+//     xhr.onreadystatechange = function(evt){
+//         if(xhr.readyState == 4 && xhr.status == 200){
+//             console.log(xhr.re)
+//         }
+//     }
+// }

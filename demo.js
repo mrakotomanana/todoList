373f6a20fbafcde7fe64a40fc9ba449838407efb
mrakotomanana <rakotomanan@gmail.com>
2023-03-09 20:44:03 +0100

    add index

--- demo.js
+++
@@ -1,9 +0,0 @@
-// var ajax = function(url){
-//     var xhr = XMLHttpRequest()
-//     xhr.open('GET', url, true)
-//     xhr.onreadystatechange = function(evt){
-//         if(xhr.readyState == 4 && xhr.status == 200){
-//             console.log(xhr.re)
-//         }
-//     }
-// }
